529ca867529d101533eebd83940ca0930ce0221f
michael-mccrickard <michael@mccrickard.net>
2024-01-25 09:24:56 -0500

    Update main.js
    Failed attempt to install bootstrap created errors when running dev server.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './styles.css';
+
+//import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap"
 
 const app = createApp(App)
 
